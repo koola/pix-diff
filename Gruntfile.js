66b66f7f4315615b176f29b9c2279d20339c8af7
koola <koola@users.noreply.github.com>
2016-11-08 03:40:15 +0800

    Update config

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,14 +9,14 @@ module.exports = function(grunt) {
         clean: {
             screens: {
                 src: [
-                    'test/screenshots/*',
-                    '!test/screenshots/exampleFail*.png'
+                    'test/baseline/',
+                    'test/diff'
                 ]
             }
         },
 
         jshint: {
-            all: ['index.js', 'test/**/*.spec.js', 'test/**/*.steps.js'],
+            all: ['index.js', 'test/*.spec.js'],
             options: {
                 jshintrc: '.jshintrc',
                 ignores: ['node_modules/', 'framework/']
@@ -24,34 +24,16 @@ module.exports = function(grunt) {
         },
 
         run: {
-            cucumber: {
+            local: {
                 cmd: 'node_modules/.bin/protractor',
                 args: [
-                    'test/protractorCucumber.conf.js'
+                    'test/conf/protractor.local.conf.js'
                 ]
             },
-            jasmine: {
+            saucelabs: {
                 cmd: 'node_modules/.bin/protractor',
                 args: [
-                    'test/protractorJasmine.conf.js'
-                ]
-            },
-            mocha: {
-                cmd: 'node_modules/.bin/protractor',
-                args: [
-                    'test/protractorMocha.conf.js'
-                ]
-            },
-            ios: {
-                cmd: 'node_modules/.bin/protractor',
-                args: [
-                    'test/appium/protractorSimulatorIOS.conf.js'
-                ]
-            },
-            android: {
-                cmd: 'node_modules/.bin/protractor',
-                args: [
-                    'test/appium/protractorEmulatorAndroid.conf.js'
+                    'test/conf/protractor.saucelabs.conf.js'
                 ]
             }
         },
@@ -73,12 +55,9 @@ module.exports = function(grunt) {
     });
 
     //tasks
-    grunt.registerTask('cucumber', 'Run cucumber integration tests', ['clean:screens', 'run:cucumber']);
-    grunt.registerTask('jasmine', 'Run Jasmine integration tests', ['clean:screens', 'run:jasmine']);
-    grunt.registerTask('mocha', 'Run Mocha integration tests', ['clean:screens', 'run:mocha']);
-    grunt.registerTask('ios', 'Run Appium tests on Saucelabs', ['clean:screens', 'run:ios']);
-    grunt.registerTask('android', 'Run Appium tests on Saucelabs', ['clean:screens', 'run:android']);
+    grunt.registerTask('local', 'Run desktop tests on local', ['clean:screens', 'run:local']);
+    grunt.registerTask('saucelabs', 'Run all tests on Saucelabs', ['clean:screens', 'run:saucelabs']);
     grunt.registerTask('build', ['jshint:all']);
     grunt.registerTask('release', ['bump']);
-    grunt.registerTask('default', ['jasmine', 'mocha', 'cucumber', 'ios']);
+    grunt.registerTask('default', ['local']);
 };
