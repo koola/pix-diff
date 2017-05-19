4a0cb4dceeec534fc950d24d6d25fdf71b50244b
koola <koola@users.noreply.github.com>
2017-05-19 14:07:11 +0800

    test(local): update chrome arguments

--- test/conf/protractor.local.conf.js
+++ test/conf/protractor.local.conf.js
@@ -12,26 +12,10 @@ config.multiCapabilities = [
     {
         browserName: 'chrome',
         logName: 'Chrome',
-        maxInstances: 2,
-        shardTestFiles: true,
         chromeOptions: {
-            args: [
-                'enable-automation',
-                '--disable-infobars',
-                '--disable-cache',
-                '--disable-application-cache',
-                '--disable-offline-load-stale-cache',
-                '--disk-cache-size=0',
-                '--v8-cache-options=off'
-            ]
+            args: ['disable-infobars']
         }
     }
-//    {
-//        browserName: 'firefox',
-//        logName: 'Firefox',
-//        maxInstances: 2,
-//        shardTestFiles: true
-//    }
 ];
 
 exports.config = config;
